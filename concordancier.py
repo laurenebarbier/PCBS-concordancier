68c48212b9274d64b324b7c6e49e49612bdd7229
laurene barbier <laurenebarbier@hotmail.fr>
2018-12-21 13:51:46 +0100

    structure

--- concordancier.py
+++ concordancier.py
@@ -3,10 +3,27 @@
 ""
 
 import pandas as pd
-import string 
+import string
 
-lex = pd.read_csv("x", sep='\t')
+#enlève la ponctuation du texte utilisé
+def remove_punctuation(text):
+    punct = string.punctuation + chr(10)
+    return text.translate(str.maketrans(punct, " " * len(punct)))
+
+#recherche une chaine de caractère
+STRING = 'x'
+FILENAME = 'x'
+f = open(FILENAME, 'r', encoding='utf-8')
+for line in f:
+    if STRING in line:
+        print(line, end='')
 
 
+#corps du programme
 if __name__ == '__main__':
         main()
+
+textori = open('alice.txt').read().lower()
+text = remove_punctuation(textori)
+words = text.split()
+print(words)
