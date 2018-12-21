c5311589fc7d9b22900bee94fbe3fc4af0f9dcaf
laurene barbier <laurenebarbier@hotmail.fr>
2018-12-21 13:27:27 +0100

    essai

--- concordancier.py
+++ concordancier.py
@@ -1,2 +1,12 @@
 # Time-stamp: <2018-12-07 14:55>
 # Projet de concordancier multilingue
+""
+
+import pandas as pd
+import string 
+
+lex = pd.read_csv("x", sep='\t')
+
+
+if __name__ == '__main__':
+        main()
